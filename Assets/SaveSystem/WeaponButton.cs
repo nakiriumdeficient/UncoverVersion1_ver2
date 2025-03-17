47d5cba04ce504743219c16321d1590d7661d587
nakiriumdeficient <erongimena@gmail.com>
2025-03-17 15:14:45 +0800

    UPGRADE FULL PARTIALLY WORKING REAL
    only the weapon icon appears to not show

--- Assets/SaveSystem/WeaponButton.cs
+++ Assets/SaveSystem/WeaponButton.cs
@@ -8,6 +8,9 @@ public class WeaponButton : MonoBehaviour
 {
     public TextMeshProUGUI weaponNameText;
     public TextMeshProUGUI weaponDamageText;
+    public TextMeshProUGUI upgradeCostText; // New UI element for upgrade cost
+    public TextMeshProUGUI damageIncreaseText; // New UI element for damage increase
+    public Image weaponIconImage;
     public Button upgradeButton;
 
     private WeaponData weaponData;
@@ -28,6 +31,14 @@ public class WeaponButton : MonoBehaviour
         weaponData = weapon;
         weaponNameText.text = weapon.weaponName;
         weaponDamageText.text = "Damage: " + weapon.damage;
+        upgradeCostText.text = "Upgrade Cost: " + weaponData.upgradeCost + " Orbs"; // Show upgrade cost
+        damageIncreaseText.text = "Damage + " + weaponData.damageIncrease; // Show how much damage will increase
+
+        if (weapon.weaponIcon != null)
+        {
+            weaponIconImage.sprite = weapon.weaponIcon;
+        }
+
         upgradeButton.onClick.AddListener(UpgradeWeapon);
         Debug.Log("Button created for: " + weapon.weaponName); // Debugging message
 
@@ -35,13 +46,15 @@ public class WeaponButton : MonoBehaviour
 
     void UpgradeWeapon()
     {
-        if (GameManager.Instance.upgradeOrb > 0)
+        if (GameManager.Instance.upgradeOrb >= weaponData.upgradeCost)
         {
-            GameManager.Instance.upgradeOrb--; // Spend an upgrade orb
-            weaponData.damage += 5; // Increase weapon damage
-            weaponDamageText.text = "Damage: " + weaponData.damage;
+            GameManager.Instance.upgradeOrb -= weaponData.upgradeCost; // Spend upgrade orbs
+            weaponData.damage += weaponData.damageIncrease; // Increase weapon damage
 
-            // Update orb count in UI
+            weaponDamageText.text = "Damage: " + weaponData.damage;
+            upgradeCostText.text = "Upgrade Cost: " + weaponData.upgradeCost + " Orbs";
+            damageIncreaseText.text = "Damage + " + weaponData.damageIncrease;
+
             FindObjectOfType<SavePoint>().orbText.text = "Upgrade Orbs: " + GameManager.Instance.upgradeOrb;
 
             Debug.Log(weaponData.weaponName + " upgraded! New Damage: " + weaponData.damage);
