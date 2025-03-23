87e73bf8178efb5f0354ffb55b24300cb7faf48a
nakiriumdeficient <erongimena@gmail.com>
2025-03-23 16:54:50 +0800

    hotbar UI working.
    pa edit nlng

--- Assets/Characters/Grey/newweaponscript/WeaponPickup2.cs
+++ Assets/Characters/Grey/newweaponscript/WeaponPickup2.cs
@@ -15,6 +15,7 @@ public class WeaponPickup2 : MonoBehaviour
         if (other.CompareTag("GreyPlayer")) // Ensure only player can pick up
         {
             GameManager.Instance.CollectWeapon(weaponName, damage, upgradeCost, damageIncrease, weaponIcon);
+            GameObject.FindObjectOfType<HotBarUI>().UpdateHotbar();
             Debug.Log("Picked up: " + weaponName);
 
             Destroy(gameObject); // Remove from scene
---
+++ Assets/UI/Scripts/HotBarUI.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HotBarUI : MonoBehaviour
+{
+    public List<Image> weaponSlots; // UI slot images
+    public Sprite emptySlotSprite;  // Default icon for empty slots
+    public Color activeColor = Color.white;   // Bright color for equipped weapon
+    public Color inactiveColor = new Color(1, 1, 1, 0.5f);  // Dimmed color for others
+
+    private void Start()
+    {
+        UpdateHotbar();
+    }
+
+    public void UpdateHotbar()
+    {
+        List<WeaponData> weapons = GameManager.Instance.collectedWeapons;
+        int equippedIndex = GameManager.Instance.GetEquippedWeaponIndex(); // Get equipped weapon index
+        for (int i = 0; i < weaponSlots.Count; i++)
+        {
+            if (i < weapons.Count)
+            {
+                weaponSlots[i].sprite = weapons[i].weaponIcon; // Set collected weapon icon
+            }
+            else
+            {
+                weaponSlots[i].sprite = emptySlotSprite; // Show empty slot
+            }
+        }
+    }
+}
